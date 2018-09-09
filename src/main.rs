5eb87efe0308fbe1329f0a1ebb4bf810fad13eae
Daniel Cassidy <mail@danielcassidy.me.uk>
2018-09-09 03:48:40 +0100

    Add conditional extern crate declarations.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+#[cfg(all(windows, not(feature = "vulkan")))]
+extern crate gfx_backend_dx12 as gfx_backend;
+#[cfg(target_os = "macos")]
+extern crate gfx_backend_metal as gfx_backend;
+#[cfg(any(all(not(windows), not(target_os = "macos")), all(windows, feature = "vulkan")))]
+extern crate gfx_backend_vulkan as gfx_backend;
+extern crate gfx_hal;
+extern crate winit;
+
 fn main() {
     println!("Hello, world!");
 }
