da36187bcaa60c342fb0fd1a53234c100502bfc7
Daniel Cassidy <mail@danielcassidy.me.uk>
2018-10-08 18:34:16 +0100

    Acquire graphics command pool.

--- src/renderer.rs
+++ src/renderer.rs
@@ -1,8 +1,11 @@
+use gfx_hal::command::OneShot;
 use gfx_hal::pool::CommandPool;
 use gfx_hal::queue::capability::Graphics;
 use gfx_hal::Backend;
 
 pub fn render<B: Backend>(
-    graphics_command_pool: CommandPool<B, Graphics>,
+    graphics_command_pool: &mut CommandPool<B, Graphics>,
 ) {
+    let mut graphics_command_buffer =
+        graphics_command_pool.acquire_command_buffer::<OneShot>(false);
 }
