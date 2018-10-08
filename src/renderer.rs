3c699614428cfc87fe2f152fc72a44d9d4ec1ad7
Daniel Cassidy <mail@danielcassidy.me.uk>
2018-10-08 18:32:34 +0100

    Remove transfer command pool from render stub.

--- src/renderer.rs
+++ src/renderer.rs
@@ -1,10 +1,8 @@
 use gfx_hal::pool::CommandPool;
 use gfx_hal::queue::capability::Graphics;
-use gfx_hal::queue::capability::Transfer;
 use gfx_hal::Backend;
 
 pub fn render<B: Backend>(
-    transfer_command_pool: CommandPool<B, Transfer>,
     graphics_command_pool: CommandPool<B, Graphics>,
 ) {
 }
